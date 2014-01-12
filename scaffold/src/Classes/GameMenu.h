b5c5f515c392e16c716ce4a288aeb03d68cad2c5
richardison <richard.ison@carleton.ca>
2014-01-12 15:46:32 -0500

    Richard-Menu

---
+++ scaffold/src/Classes/GameMenu.h
@@ -0,0 +1,13 @@
+//
+//  GameMenu.h
+//  3004iPhone
+//
+//  Created by Richard Ison on 1/12/2014.
+//
+//
+
+#import "SPStage.h"
+
+@interface GameMenu : SPStage
+
+@end
---
+++ scaffold/src/Classes/GameMenu.m
@@ -0,0 +1,13 @@
+//
+//  GameMenu.m
+//  3004iPhone
+//
+//  Created by Richard Ison on 1/12/2014.
+//
+//
+
+#import "GameMenu.h"
+
+@implementation GameMenu
+
+@end
