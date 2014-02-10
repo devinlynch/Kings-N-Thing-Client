2c189859025f5fa9208e26b70ca2b3384776355b
devinlynch <devin.lynch.is@gmail.com>
2014-02-10 12:55:49 -0500

    Done movement

--- scaffold/src/Classes/BoardLocation.m
+++ scaffold/src/Classes/BoardLocation.m
@@ -50,6 +50,7 @@
 
         }
     return self;
+    }
 }
 
 
--- scaffold/src/Classes/MovementPhaseHandler.m
+++ scaffold/src/Classes/MovementPhaseHandler.m
@@ -12,23 +12,79 @@
 #import "GameState.h"
 #import "Game.h"
 #import "Stack.h"
+#import "GameResource.h"
+#import "GamePiece.h"
 
 @implementation MovementPhaseHandler
 
+
+/*
+ Type: yourTurnToMoveInMovement
+ Data: {
+ }
+ Sending Type: UDP from server to client
+ Description: Tells the client its their turn to make a move in movement phase
+ */
 -(void) handleYourTurnToMoveInMovement: (Event*) event{
     NSLog(@"Succesfully handled yourTurnToMoveInMovement message");
     [[NSNotificationCenter defaultCenter] postNotificationName:@"yourTurnToMoveInMovement" object:nil];
 }
 
+
+/*
+ Type: playerMovedStackToNewLocation
+ Data: {
+ playerId: id of player who made move
+ hexLocationId: id of hex where stack was moved to
+ stack: serialized stack object that was moved
+ }
+ Sending Type: UDP from server to client
+ Description: Tells all players that a player moved a stsack somewhere
+*/
 -(void) handlePlayerMovedStackToNewLocation:(Event *)event{
+    [self handleUpdatedStackFromEvent:event];
+}
+
+/**
+ Type: playerCreatedStack
+ Data: {
+ playerId: id of player who created the stack
+ stack: serialized stack object that was created
+ }
+ Sending Type: UDP from server to client
+ Description: Tells all players that a player created a stack that should now be on a hex location
+ */
+
+-(void) handlePlayerCreatedStack:(Event *)event{
+    [self handleUpdatedStackFromEvent:event];
+}
+
+
+/*
+ Type: playerAddedPiecesToStack
+ Data: {
+ playerId: id of player who added pieces to the stack
+ stack: serialized stack object that updated
+ }
+ Sending Type: UDP from server to client
+ Description: Tells all players that a player updated the pieces in a stack
+ */
+
+-(void) handlePlayerAddedPiecesToStack:(Event *)event{
+    [self handleUpdatedStackFromEvent: event];
+}
+
+
+// Used by the stack update events
+-(void) handleUpdatedStackFromEvent: (Event*) event {
     NSDictionary* dataDic = [Utils getDataDictionaryFromGameMessageEvent:event];
     if(dataDic == nil){
         return;
     }
     
     NSString *playerId = [dataDic objectForKey:@"playerId"];
-    NSString *hexLocationId = [dataDic objectForKey:@"hexLocationId"];
     NSDictionary *stackDic = [dataDic objectForKey:@"stack"];
+    NSString *hexLocationId = [stackDic objectForKey:@"hexLocationId"];
     NSString *stackId = [stackDic objectForKey:@"locationId"];
     
     GameState *gameState = [[Game currentGame] gameState];
@@ -43,10 +99,55 @@
     HexLocation *hexLocation = (HexLocation*)[gameState getBoardLocationById:hexLocationId];
     [stack setLocation:hexLocation];
     
-
-    NSLog(@"Succesfully parsed playerMovedStackToNewLocation message with stackID: %@", stack.locationId);
     
-    [[NSNotificationCenter defaultCenter] postNotificationName:@"playerMovedStackToNewLocation" object:hexLocation];
+    NSLog(@"Succesfully parsed updated stack message with stackID: %@", stack.locationId);
+    
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"playerUpdatedStack" object:stack];
 }
+
+
+/*
+ Type: playerMovedPieceToNewLocation
+ Data: {
+ playerId: id of player who moved the piece
+ boardLocationId: id of the board location that the piece is being moved to
+ gamePieceId: if of the game piece being moved
+ }
+ Sending Type: UDP from server to client
+ Description: Tells all players that a player moved a game piece
+ */
+
+-(void) handlePlayerMovedPieceToNewLocation: (Event*) event {
+    NSDictionary* dataDic = [Utils getDataDictionaryFromGameMessageEvent:event];
+    if(dataDic == nil){
+        return;
+    }
+    
+    NSString *playerId = [dataDic objectForKey:@"playerId"];
+    NSString *boardLocationId = [dataDic objectForKey:@"boardLocationId"];
+    NSString *gamePieceId = [dataDic objectForKey:@"gamePieceId"];
+    
+    GameState *gameState = [[Game currentGame] gameState];
+    
+    Player *player = [gameState getPlayerById:playerId];
+    BoardLocation *boardLocation = [gameState getBoardLocationById:boardLocationId];
+    GamePiece *gamePiece = [[GameResource getInstance] getPieceForId:gamePieceId];
+    
+    
+    if(player == nil || [player isKindOfClass:[NSNull class]]
+       || boardLocation == nil || [boardLocation isKindOfClass:[NSNull class]]
+       || gamePiece == nil || [gamePiece isKindOfClass:[NSNull class]]) {
+        NSLog(@"Could not find player or boardlocation or gamepiece in handlePlayerMovedPieceToNewLocation.  The params where: %@", dataDic);
+        return;
+    }
+    
+    [boardLocation addGamePieceToLocation:gamePiece];
+
+    
+    NSLog(@"Succesfully parsed playerMovedPieceToNewLocation with gamepieceid: %@ and locationid: %@", gamePiece.gamePieceId, gamePiece.location.locationId);
+    
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"playerMovedPieceToNewLocation" object:gamePiece];
+}
+
 
 @end
