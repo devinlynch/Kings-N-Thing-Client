1347a34c33f3d3c78d8710fe39e2b0ca8145ba04
devinlynch <devin.lynch.is@gmail.com>
2014-03-29 14:20:19 -0400

    Added handler for combat

---
+++ scaffold/src/Classes/CombatPhaseHandler.h
@@ -0,0 +1,13 @@
+//
+//  CombatPhaseHandler.h
+//  3004iPhone
+//
+//  Created by Devin Lynch on 2014-03-29.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CombatPhaseHandler : NSObject
+
+@end
---
+++ scaffold/src/Classes/CombatPhaseHandler.m
@@ -0,0 +1,46 @@
+//
+//  CombatPhaseHandler.m
+//  3004iPhone
+//
+//  Created by Devin Lynch on 2014-03-29.
+//
+//
+
+#import "CombatPhaseHandler.h"
+#import "Event.h"
+
+@implementation CombatPhaseHandler
+
+-(void) handleBattleOver: (Event*) event{
+    
+}
+
+-(void) handleBattleStarted: (Event*) event{
+    
+}
+
+-(void) handleCombatPhaseStarted: (Event*) event{
+    
+}
+
+-(void) handleCombatRoundStarted: (Event*) event{
+    
+}
+
+-(void) handlePlayerRetreatedFromBattle: (Event*) event{
+    
+}
+
+-(void) handleCombatStepStarted: (Event*) event{
+    
+}
+
+-(void) handlePlayerTookDamageInBattle: (Event*) event{
+    
+}
+
+-(void) handleRetreatCouldNotTakePlace: (Event*) event{
+    
+}
+
+@end
