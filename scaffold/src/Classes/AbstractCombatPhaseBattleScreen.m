3c973b5fc00f8c01c41951456715aef11ca44232
devinlynch <devin.lynch.is@gmail.com>
2014-04-04 12:48:50 -0400

    Refactoring

---
+++ scaffold/src/Classes/AbstractCombatPhaseBattleScreen.m
@@ -0,0 +1,37 @@
+//
+//  AbstractCombatPhaseBattleScreen.m
+//  3004iPhone
+//
+//  Created by Devin Lynch on 2014-04-04.
+//
+//
+
+#import "AbstractCombatPhaseBattleScreen.h"
+
+@implementation AbstractCombatPhaseBattleScreen
+
+-(id) initWithBattle: (CombatBattle*) battle andController: (CombatPhaseScreenController*) contoller{
+    self = [super initFromCombatController:contoller];
+    if(self) {
+        _battle = battle;
+    }
+    return self;
+}
+
+-(void) showPiecesForBattleForMe{
+    if([_battle amIAttacker]) {
+        [self showPiecesMenuForPlayer:_battle.attacker onLocation:(BoardLocation*)_battle.locationOfBattle];
+    } else{
+        [self showPiecesMenuForPlayer:_battle.defender onLocation:(BoardLocation*)_battle.locationOfBattle];
+    }
+}
+
+-(void) showPiecesForOpposingPlayer{
+    if([_battle amIAttacker]) {
+        [self showPiecesMenuForPlayer:_battle.defender onLocation:(BoardLocation*)_battle.locationOfBattle];
+    } else{
+        [self showPiecesMenuForPlayer:_battle.attacker onLocation:(BoardLocation*)_battle.locationOfBattle];
+    }
+}
+
+@end
